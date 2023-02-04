6909704bfff7e51e1ced982996d03e736ec8336d
balaev.yr <balaev.yr@alidi.ru>
2023-02-04 19:12:17 +0500

    initial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ab = void 0;
+const ab = (a, b) => {
+    return a + b;
+};
+exports.ab = ab;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const ab = (a: number, b: number) => {
+    return a+b;
+}
